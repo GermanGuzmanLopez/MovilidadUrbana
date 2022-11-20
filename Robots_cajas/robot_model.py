0a376e31dc20e31728caefa48e45ae3914022d51
A01745597 <A01745597@itesm.mx>
2022-11-19 21:48:51 -0600

    Actividad_integradora-Robots_mesa

---
+++ Robots_cajas/robot_model.py
@@ -0,0 +1,49 @@
+import mesa
+from robot_agents import *
+
+class robot_model(mesa.Model):
+    
+    def __init__(self, n, k, width, height, max_step):
+        self.num_agents = n
+        self.grid = mesa.space.MultiGrid(width, height, False)
+        self.schedule = mesa.time.RandomActivation(self)
+        self.bschedule = mesa.time.BaseScheduler(self)
+        self.current_step = 2
+        self.max_step = max_step
+        
+        for i in range(n):
+            a = robots(i, self)
+
+            #Add the agent to a random cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(1, self.grid.height)
+
+            cellmates = self.grid.get_cell_list_contents((x, y))
+            while len(cellmates) > 0:
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+                
+                cellmates = self.grid.get_cell_list_contents((x, y))
+            self.schedule.add(a)
+            self.grid.place_agent(a, (x, y))
+
+        for i in range(k):
+            a = caja(i, self)
+
+            #Add the agent to a random cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+
+            cellmates = self.grid.get_cell_list_contents((x, y))
+            while len(cellmates) > 0:
+                x = self.random.randrange(self.grid.width)
+                y = self.random.randrange(self.grid.height)
+                cellmates = self.grid.get_cell_list_contents((x, y))
+                
+            self.bschedule.add(a)
+            self.grid.place_agent(a, (x, y))
+
+    def step(self) -> None:
+        self.running = (self.current_step < self.max_step)
+        self.schedule.step()
+        self.current_step += 1
---
+++ Robots_cajas/robot_server.py
@@ -0,0 +1,33 @@
+import mesa
+from mesa.visualization.UserParam import UserSettableParameter
+from robot_model import *
+
+def agent_portrayal(agent):
+    portrayal = {"Shape": "circle",
+                 "Filled": "true",
+                 "r": 0.5}
+
+    if agent.caja:
+        portrayal["Color"] = "brown"
+        portrayal["Layer"] = 0
+        portrayal["r"] = 0.4
+    else:
+        portrayal["Color"] = "grey"
+        portrayal["Layer"] = 1
+
+    return portrayal
+
+grid = mesa.visualization.CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+
+# chart = mesa.visualization.ChartModule([{"Label": "Dirt Percentage",
+#                       "Color": "Black"}],
+#                     data_collector_name = 'datacollector')
+
+server = mesa.visualization.ModularServer(robot_model, [grid], "Robot Model",
+    {"n": 5,
+    "k": UserSettableParameter("slider", "num_cajas", value = 50, max_value = 50, min_value = 1), 
+    "width":10, "height":10,  
+    "max_step": UserSettableParameter("slider", "Max_steps", value = 100, max_value = 500, min_value = 1)})
+
+server.port = 8521 # The default
+server.launch()
